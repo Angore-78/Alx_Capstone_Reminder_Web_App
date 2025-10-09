fc4e1b008834f00a55935e380c2c967b940320c8
Angore-78 <allanangore@gmail.com>
2025-10-09 22:18:17 +0300

    create forms.py, add views to views.py and include reminders ap to installed apps in task_manager/settings.py

--- reminders/views.py
+++ reminders/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
+from .models import Task
+from .forms import LoginForm,SignUpForm
 
-# Create your views here.
+
+def index(request,id):
+    task = Task.objects.get(id = id)
+    return render(request,'reminders/base.html',{})
+
+
+def home(request):
+    return render(request,'reminders/home.html',{})
+
+
+def tasks(request):
+    task = Task.objects.all()
+    context = {'task':task}
+    #form = 
+    return render(request,'reminders/task_list.html',context)
+
+def login(request):
+    form = LoginForm
+    return render(request,'registration/login.html',{'form':form})
+
+def sign_up(request):
+    form=SignUpForm
+    return render(request,'registration/sign_in.html')
